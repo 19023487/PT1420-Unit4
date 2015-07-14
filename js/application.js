cde6e483a35bb34ed26c863413b66eb9a5b6e027
Chris Jackson <cjackson290@email.itt-tech.edu>
2015-07-13 22:13:04 -0500

    Edit button

--- js/application.js
+++ js/application.js
@@ -8,40 +8,70 @@ function loadToDoList() {
 	}
 }
 
+function editToDo(itemToEdit) {
+	var newlist = [];
+	for(var i=0; i<toDoList.length; i++){
+		if( i === itemToEdit) {
+			var editedToDo = prompt(toDoList[i]);
+			newList.push(editedToDo);
+		} else{
+		  newList.push(toDoList[i]);
+		}	
+	}
+	toDoList =newList;
+	changeDisplay();		
+}
 function changeDisplay() {
 	var list = document.getElementById("todolist");	//ONLY PLACE WE CHANGE THE SCREEN
 	list.innerHTML = "";
 	for(var i=0; i<toDoList.length; i++){
-		list.innerHTML += '<div class="item">' + toDoList[i] + 
-		' (<a href="#" onclick="javascript:removeToDo(' + i +')">X</a>)</div>';
+		list.innerHTML += '<li class="list-group-item">' + toDoList[i] + 
+		'<div class="btn-group-xs pull-right">' +
+		'<button class="btn btn-warning delete" onclick="javascript:removeToDo(' + i +')">' + 
+		'Delete</button>' + 
+		'<button class="btn btn-warinig edit" onclick="javascript:editToDo(' + i +')">' +
+		'Edit</button>' +
+		'</div>'
+		'</li>';
 	}	
-	localStorage.todolist = JSON.stringify(toDoList);
+	localStorage.todolist = JSON.stringify(toDoList);	
 }
-
 function removeToDo(itemToRemove) {
 	var newList = [];
 	for(var i=0;i<toDoList.length;i++) {
 		if( i !== itemToRemove) {
 			newList.push(toDoList[i]);
-			
-		}
 		
 	}
 	toDoList = newList;
 	changeDisplay();
 }
 
+function editToDo() {
+	for(var i=0;i,toDoList.length;i++) {
+		list.innerHTML += '<li class="list-group-item">' + toDoList[i] + 
+		'<div class="btn-group-xs pull-right">' +
+		'<button class="btn btn-warning delete" onclick="javascript:removeToDo(' + i +')">' + 
+		'Delete</button>' + 
+		'<button class="btn btn-warinig edit" onclick="javascript:editToDo(' + i +')">' +
+		'Edit</button>' +
+		'</div>'
+		'</li>';
+	}	
+}
 
 function addToDo() {
 	var tmpItem;
-	var newToDo = document.getElementById("todonew");	tmpItem = newToDo.value;
+	var newToDo = document.getElementById("todonew");	
+	tmpItem = newToDo.value;
 	
 	if(tmpItem === "") {
 		alert("You didn't put anything in to add");
 		return;
 		
 	}
-	
+}
+
 	toDoList.push(tmpItem);
 	//Call our display function
 	changeDisplay();
