c0a07a9614a79ec6a416ccaa664184f9959ca21d
Chris Jackson <cjackson290@email.itt-tech.edu>
2015-07-15 18:50:14 -0500

    Edit button

--- js/application.js
+++ js/application.js
@@ -1,81 +1,52 @@
-//Define Global
+// Define Global
 var toDoList = [];
 
-function loadToDoList() {
-	if(localStorage.todolist) {
-	toDoList = JSON.parse(localStorage.todolist);
-	changeDisplay();
+function loadToDoList(){
+	if(localStorage.todolist){
+		toDoList = JSON.parse(localStorage.todolist);
+		changeDisplay();
 	}
 }
 
-function editToDo(itemToEdit) {
-	var newlist = [];
-	for(var i=0; i<toDoList.length; i++){
-		if( i === itemToEdit) {
-			var editedToDo = prompt(toDoList[i]);
-			newList.push(editedToDo);
-		} else{
-		  newList.push(toDoList[i]);
-		}	
-	}
-	toDoList =newList;
-	changeDisplay();		
-}
-function changeDisplay() {
-	var list = document.getElementById("todolist");	//ONLY PLACE WE CHANGE THE SCREEN
+function changeDisplay(){
+	var list = document.getElementById("todolist");
+	// Only place we actually change the screen
 	list.innerHTML = "";
 	for(var i=0; i<toDoList.length; i++){
-		list.innerHTML += '<li class="list-group-item">' + toDoList[i] + 
-		'<div class="btn-group-xs pull-right">' +
-		'<button class="btn btn-warning delete" onclick="javascript:removeToDo(' + i +')">' + 
-		'Delete</button>' + 
-		'<button class="btn btn-warinig edit" onclick="javascript:editToDo(' + i +')">' +
-		'Edit</button>' +
-		'</div>'
-		'</li>';
-	}	
-	localStorage.todolist = JSON.stringify(toDoList);	
+		list.innerHTML += '<div class="item">' + toDoList[i] +
+		' (<a href="#" onclick="javascript:removeToDo(' + i +')">X</a>)' +
+		'</div>';
+	}
+	localStorage.todolist = JSON.stringify(toDoList);
 }
-function removeToDo(itemToRemove) {
+
+function removeToDo(itemToRemove){
 	var newList = [];
-	for(var i=0;i<toDoList.length;i++) {
+	for(var i=0;i<toDoList.length;i++){
 		if( i !== itemToRemove) {
 			newList.push(toDoList[i]);
-		
+		}
 	}
 	toDoList = newList;
 	changeDisplay();
 }
 
-function editToDo() {
-	for(var i=0;i,toDoList.length;i++) {
-		list.innerHTML += '<li class="list-group-item">' + toDoList[i] + 
-		'<div class="btn-group-xs pull-right">' +
-		'<button class="btn btn-warning delete" onclick="javascript:removeToDo(' + i +')">' + 
-		'Delete</button>' + 
-		'<button class="btn btn-warinig edit" onclick="javascript:editToDo(' + i +')">' +
-		'Edit</button>' +
-		'</div>'
-		'</li>';
-	}	
-}
-
-function addToDo() {
+function addToDo(){
 	var tmpItem;
 	var newToDo = document.getElementById("todonew");	
 	tmpItem = newToDo.value;
 	
-	if(tmpItem === "") {
+	if(tmpItem === ""){
 		alert("You didn't put anything in to add");
 		return;
-		
 	}
-}
 
 	toDoList.push(tmpItem);
-	//Call our display function
+	// Call our display function
 	changeDisplay();
 	
 	newToDo.value = "";
 	newToDo.select();
-}
+} 
+
+  
